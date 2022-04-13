f43e379509ed1601925897b731df7bf77c0a7142
Alex <alexson2302@gmail.com>
2022-04-14 02:26:19 +0300

    lesson-2

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import products
 
 urlpatterns = [
-    path('', products),
+    path('', products, name='products'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -2,6 +2,17 @@ from django.shortcuts import render
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    links_menu = [
+        {'href': 'products_all', 'name': 'все'},
+        {'href': 'products_home', 'name': 'дом'},
+        {'href': 'products_office', 'name': 'офис'},
+        {'href': 'products_modern', 'name': 'модерн'},
+        {'href': 'products_classic', 'name': 'классика'},
+    ]
+
+    context = {
+        'links_menu':links_menu
+    }
+    return render(request, 'mainapp/products.html', context=context)
 
 # Create your views here.
