34cef43379b2cffce9dc4966217dbeab7c7326b2
Alex <alexson2302@gmail.com>
2022-04-22 02:38:04 +0300

    lesson-4

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -25,8 +24,7 @@ SECRET_KEY = 'django-insecure-y!m#=s*=z9%ycjw@9575o%12j4z-kj*+e_3bm(j^)z=_a8b!0s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -39,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'mainapp',
+    'authapp',
 ]
 
 MIDDLEWARE = [
@@ -71,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'geekshop.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -81,7 +79,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -101,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -112,7 +108,6 @@ TIME_ZONE = 'Europe/Moscow'
 USE_I18N = True
 
 USE_TZ = False
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
@@ -128,9 +123,10 @@ STATICFILES_DIRS = (
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-#Media files
+# Media files
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = "authapp.ShopUser"
 
---
+++ mainapp/management/commands/fill_db.py
@@ -0,0 +1,57 @@
+import json
+import os
+
+from django.core.management.base import BaseCommand
+
+from authapp.models import ShopUser
+from mainapp.models import ProductCategory, Product
+
+JSON_PATH = 'mainapp/jsons'
+
+
+def load_from_json(file_name):
+    with open(os.path.join(JSON_PATH, file_name + '.json'), 'r', encoding='utf-8') as infile:
+        return json.load(infile)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        categories = load_from_json('categories')
+
+        ProductCategory.objects.all().delete()
+        for category in categories:
+            new_category = ProductCategory(**category)
+            new_category.save()
+
+        products = load_from_json('products')
+
+        Product.objects.all().delete()
+        for product in products:
+            category_name = product["category"]
+            _category = ProductCategory.objects.get(name=category_name)
+            product["category"] = _category
+            new_product = Product(**product)
+            new_product.save()
+
+        ShopUser.objects.create_superuser('admin', 'admin@admin.com', '123', age='20')
+
+
+# class Command(BaseCommand):
+#     def handle(self, *args, **options):
+#         categories = load_from_json('categories')
+#
+#         ProductCategory.objects.all().delete()
+#         for category in categories:
+#             new_category = ProductCategory(**category)
+#             new_category.save()
+#
+#         products = load_from_json('products')
+#
+#         Product.objects.all().delete()
+#         for product in products:
+#             category_name = product["category"]
+#             _category = ProductCategory.objects.get(name=category_name)
+#             product['category'] = _category
+#             new_product = Product(**product)
+#             new_product.save()
+#         User.objects.create_superuser('django', 'django@geekshop.local', 'geekbrains')
